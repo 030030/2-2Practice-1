34cb8da2d510b6e8ba95e7a405c9be65d794304f
030030 <74403092+030030@users.noreply.github.com>
2021-03-17 00:38:15 +0800

    Update GenerateBomb.aspx.cs

--- 2-2Practice/GenerateBomb.aspx.cs
+++ 2-2Practice/GenerateBomb.aspx.cs
@@ -16,45 +16,64 @@ namespace _2_2Practice
         protected void Page_Load(object sender, EventArgs e)
         {
             int[] bomb = new int[10] { 0,7,13,28, 44, 62, 74, 75, 87,90 };
-            int x=0, y=0;
-            int[,] bomb2 = new int[10, 10];
+            int x=0, y=0,z=0;
+            string[,] bomb2 = new string[10, 10];
+            int[,] bomb3 = new int[10, 10];
             Response.Write("<table border=4>");
             for (int i = 0; i < bomb.Length; i++) {
                 bombxy(ref x, ref y, bomb[i]);
-                bomb2[x, y] = -1;
+                bomb2[x, y] = "*";
                 if ((x - 1) >= 0)
                 {
                     if ((y - 1) >= 0)
                     {
-                        bomb2[x-1, y-1] += 1;
+                        bomb3[x - 1, y - 1] +=1;
+                        z = bomb3[x - 1, y - 1] ;
+                        bomb2[x-1, y-1] =z.ToString();
                     }
                     if ((y + 1) <= 9)
                     {
-                        bomb2[x - 1, y + 1] += 1;
+                        bomb3[x - 1, y + 1] += 1;
+                        z = bomb3[x - 1, y + 1] ;
+                        bomb2[x - 1, y + 1] = z.ToString();
                     }
-                    bomb2[x - 1, y] += 1;
-                }if ((x + 1) <= 9){
+                    bomb3[x - 1, y] += 1;
+                    z = bomb3[x - 1, y ] ;
+                    bomb2[x - 1, y] = z.ToString();
+                }
+                if ((x + 1) <= 9){
                     if ((y - 1) >= 0)
                     {
-                        bomb2[x + 1, y - 1] += 1;
+                        bomb3[x + 1, y - 1] += 1;
+                        z = bomb3[x + 1, y - 1] ;
+                        bomb2[x + 1, y - 1] = z.ToString();
                     }
                     if ((y + 1) <= 9)
                     {
-                        bomb2[x + 1, y +1] += 1;
+                        bomb3[x + 1, y + 1] += 1;
+                        z = bomb3[x + 1, y + 1] ;
+                        bomb2[x + 1, y +1] = z.ToString();
                     }
-                    bomb2[x + 1, y] += 1;
+                    bomb3[x + 1, y] += 1;
+                    z = bomb3[x + 1, y ] ;
+                    bomb2[x + 1, y] = z.ToString();
                 }
                 if ((y - 1) >= 0)
                 {
-                    bomb2[x , y - 1] += 1;
-                }if ((y + 1) <= 9)
+                    bomb3[x, y - 1] +=1;
+                    z = bomb3[x , y - 1] ;
+                    bomb2[x , y - 1] = z.ToString();
+                }
+                if ((y + 1) <= 9)
                 {
-                    bomb2[x, y + 1] += 1;
+                    bomb3[x, y + 1] += 1;
+                    z = bomb3[x , y + 1] ;
+                    bomb2[x, y + 1] = z.ToString();
                 }
             }
             for (int i = 0; i < bomb.Length; i++) {
                 bombxy(ref x, ref y, bomb[i]);
-                bomb2[x, y] = -1;
+                bomb2[x, y] = "*";
             }
             for (x = 0; x < 10; x++) {
                 Response.Write("<tr>");
